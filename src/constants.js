7acac513954cbe85c0646fb5f17ab71922a48ba1
jerryrox <jerryrox@live.co.kr>
2018-06-19 21:05:06 +1200

    Removed some fields

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,6 @@ const HITOMI_CHAN_READ = "http://localhost:4000/read/";
 const HITOMI_CHAN_ORIGINAL = "http://localhost:4000/original/";
 const HITOMI_CHAN_THUMB_BIG = "http://localhost:4000/thumbs/big/";
 const HITOMI_CHAN_THUMB_SMALL = "http://localhost:4000/thumbs/small/";
-const HITOMI_CHAN_THUMB_BIG_META = "http://localhost:4000/thumbs/big/meta/";
-const HITOMI_CHAN_THUMB_SMALL_META = "http://localhost:4000/thumbs/small/meta/";
 
 module.exports = {
     HITOMI_CHAN_BASE,
@@ -13,7 +11,5 @@ module.exports = {
     HITOMI_CHAN_READ,
     HITOMI_CHAN_ORIGINAL,
     HITOMI_CHAN_THUMB_BIG,
-    HITOMI_CHAN_THUMB_SMALL,
-    HITOMI_CHAN_THUMB_BIG_META,
-    HITOMI_CHAN_THUMB_SMALL_META,
+    HITOMI_CHAN_THUMB_SMALL
 };
