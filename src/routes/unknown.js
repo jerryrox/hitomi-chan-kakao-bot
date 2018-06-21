0c2f8f350a40699059c3b44cf98d45d3cd02c7ec
jerryrox <jerryrox@live.co.kr>
2018-06-21 23:22:58 +1200

    Fixed bug

--- src/routes/unknown.js
+++ src/routes/unknown.js
@@ -3,14 +3,17 @@ const router = express.Router();
 
 router.get("*", (req, res) => {
     console.log(`Unknown GET request to ${req.originalUrl}`);
+    res.send({});
 });
 
 router.post("*", (req, res) => {
     console.log(`Unknown POST request to ${req.originalUrl}`);
+    res.send({});
 });
 
 router.delete("*", (req, res) => {
     console.log(`Unknown DELETE request to ${req.originalUrl}`);
+    res.send({});
 });
 
 module.exports = router;
